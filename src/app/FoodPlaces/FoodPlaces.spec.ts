99029f249ec7a6f6369ec4a4711b640a79679285
bpl <benlafrance42@gmail.com>
2024-05-19 13:33:56 -0700

    set up client for deployment

---
+++ src/app/FoodPlaces/FoodPlaces.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FoodPlacesComponent } from './FoodPlaces.component';
+
+describe('FoodPlacesComponent', () => {
+  let component: FoodPlacesComponent;
+  let fixture: ComponentFixture<FoodPlacesComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [FoodPlacesComponent]
+    })
+    .compileComponents();
+    
+    fixture = TestBed.createComponent(FoodPlacesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+// import { HelloComponent } from './hello/hello.component';
+import { FoodPlacesComponent } from './FoodPlaces/FoodPlaces.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    // {path: '', component: HelloComponent, pathMatch: 'full'},
+    {path: 'FoodPlaces', component: FoodPlacesComponent}
+];
